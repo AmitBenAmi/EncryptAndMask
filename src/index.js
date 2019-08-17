542750eeb63c0e84b0c052d4695370b69ed62fbf
AmitBenAmi <benamiamit0@gmail.com>
2019-08-17 19:51:57 +0300

    Fixing the 404 error with the favicon.ico

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname, '../../public')))
 
+app.get('favicon.ico', (req, res) => {
+    res.status(204)
+})
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`)
 })
