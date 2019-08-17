2ad9297fbbef51820b53effb0a170c83e700ec4a
AmitBenAmi <benamiamit0@gmail.com>
2019-08-17 19:38:30 +0300

    Listening to specific port

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import path from 'path'
 import express from 'express'
 const app = express()
+const port = 3000
 
 app.use(express.static(path.join(__dirname, '../public')))
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}.`)
+})
