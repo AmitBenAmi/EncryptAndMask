1413a07f233d24feb3b9b4b3815dc272d5a01008
AmitBenAmi <benamiamit0@gmail.com>
2019-08-26 22:48:21 +0300

    Fixing the bug

--- src/ecdhAlgo.js
+++ src/ecdhAlgo.js
@@ -1,24 +1,21 @@
-import bigInt from 'big-integer'
 import crypto from 'crypto'
-
-const p = new bigInt(7577) // Must be prime
-const g = new bigInt(1367)
-const a = 16
-const A = g.pow(a).mod(p)
 
 class ECDHAlgorithm {
     constructor() {
         this.ecdh = crypto.createECDH('secp256k1')
         this.ecdh.generateKeys()
+
+        this.otherEcdh = crypto.createECDH('secp256k1')
+        this.otherEcdh.generateKeys()
     }
     
     get public() {
         return keyPair.getPublicKey()
     }
-    
-    calculateKey_a = (B) => {
-        return new bigInt(B).pow(a).mod(p)
+
+    decrypt(publicKey) {
+        this.ecdh.decrypt
     }
 }
 
-export default DHAlgorithm
+export default ECDHAlgorithm
